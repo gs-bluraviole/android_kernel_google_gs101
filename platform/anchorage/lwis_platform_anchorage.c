9690bb66c7e62b83a7111c4ba59d0019d1ab019f
Meghana Barkalle <mbarkalle@google.com>
2024-10-24 23:51:22 +0000

    LWIS: anchorage: Update DPM logging level
    Add a module parameter to enable DPM update debug logs to prevent
    log spam.
    adb shell "echo 0/1 > /sys/module/lwis/parameters/lwis_anchorage_debug"
    
    Test: Make sure the log is not printed unless module parameter is set
    Bug: 375215923
    
    Signed-off-by: Meghana Barkalle <mbarkalle@google.com>
    (cherry picked from commit 99028955ccdc0d33e3d23309468e85b20e318a1f)
    (cherry picked from https://partner-android-review.googlesource.com/q/commit:c41c48d23e6887e3bb3ec1f4ef4f5efd62a4354f)
    Merged-In: Ib776910dcb222d7cc4d72a2fbf0b319e6f91e5e5
    Change-Id: Ib776910dcb222d7cc4d72a2fbf0b319e6f91e5e5

--- platform/anchorage/lwis_platform_anchorage.c
+++ platform/anchorage/lwis_platform_anchorage.c
@@ -22,6 +22,14 @@
 #include "lwis_debug.h"
 #include "lwis_platform.h"
 
+/*
+ * module_param macro defines the parameter name, type and permission bits
+ * in sysfs file system. 0644 represents that the owner can read and write the kernel
+ * parameter using command line while other users can read it.
+ */
+bool lwis_anchorage_debug;
+module_param(lwis_anchorage_debug, bool, 0644);
+
 /* Uncomment to let kernel panic when IOMMU hits a page fault. */
 /* #define ENABLE_PAGE_FAULT_PANIC */
 
@@ -251,8 +259,10 @@ int lwis_platform_update_qos(struct lwis_device *lwis_dev, int value, int32_t cl
 	else
 		exynos_pm_qos_update_request(qos_req, value);
 
-	dev_info(lwis_dev->dev, "Updating clock for clock_family %d, freq to %u\n", clock_family,
-		 value);
+	if (lwis_anchorage_debug) {
+		dev_info(lwis_dev->dev, "Updating clock for clock_family %d, freq to %u\n",
+			 clock_family, value);
+	}
 
 	return 0;
 }
@@ -402,7 +412,7 @@ int lwis_platform_update_bts(struct lwis_device *lwis_dev, int block, unsigned i
 	ret = bts_update_bw(bts_index, bts_request);
 	if (ret < 0) {
 		dev_err(lwis_dev->dev, "Failed to update bandwidth to bts, ret: %d\n", ret);
-	} else {
+	} else if (lwis_anchorage_debug) {
 		dev_info(
 			lwis_dev->dev,
 			"Updated bandwidth to bts for device %s block %s: peak: %u, read: %u, write: %u, rt: %u\n",
