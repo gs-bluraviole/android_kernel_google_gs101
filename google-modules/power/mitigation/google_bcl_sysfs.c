cf48ad2d56493005c2220a5da8ba33a5548bc328
Adam W. Willis <return.of.octobot@gmail.com>
2024-11-17 15:41:06 -0500

    google-modules: power: mitigation: Use valid format string to avoid zero-length argument
    This resolves the following compilation error:
    
    ../google-modules/power/mitigation/google_bcl_sysfs.c: In function 'trigger_br_stats_show':
    ../google-modules/power/mitigation/google_bcl_sysfs.c:4148:32: error: zero-length gnu_printf format string [-Werror=format-zero-length]
     4148 |         return sysfs_emit(buf, "");
          |                                ^~
    
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- google-modules/power/mitigation/google_bcl_sysfs.c
+++ google-modules/power/mitigation/google_bcl_sysfs.c
@@ -4225,7 +4225,7 @@ static DEVICE_ATTR_RW(enable_br_stats);
 
 static ssize_t trigger_br_stats_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
-	return sysfs_emit(buf, "");
+	return sysfs_emit(buf, "%s", "");
 }
 
 static ssize_t trigger_br_stats_store(struct device *dev, struct device_attribute *attr,
