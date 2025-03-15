0ctobot/neutrino_kernel_google_caimito

kbuild: Define KCONFIG_EXT_{MODULES_}PREFIX and KCONFIG_SOC_GS_PREFIX
Necessary to fix up the Kconfigs that depend upon these variables being
defined in the environment. Define them in order to fix Google's Kconfigs
for in-tree compilation.

Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

Makefile
@@ -5,6 +5,10 @@ SUBLEVEL = 84
 EXTRAVERSION =
 NAME = Curry Ramen
 
+export KCONFIG_EXT_PREFIX := google-devices/caimito/
+export KCONFIG_SOC_GS_PREFIX := google-modules/soc/gs/
+export KCONFIG_EXT_MODULES_PREFIX := ./
+
 ifeq ($(MAKECMDGOALS),)
 MAKECMDGOALS := Image.lz4 dtbs
 endif

gs-bluraviole/android_kernel_google_gs101

Makefile
@@ -5,6 +5,10 @@ SUBLEVEL = 130
 EXTRAVERSION =
 NAME = Curry Ramen
 
+export KCONFIG_EXT_PREFIX := google-devices/raviole/
+export KCONFIG_SOC_GS_PREFIX := google-modules/soc/gs/
+export KCONFIG_EXT_MODULES_PREFIX := ./
+
 ifeq ($(MAKECMDGOALS),)
 MAKECMDGOALS := Image.lz4 dtbs
 endif