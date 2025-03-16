0c0d1facd7939d40138747573329140a8bd2e844
Rick Yiu <rickyiu@google.com>
2025-03-16 14:59:42 +0000

    vh: sched: Increase the rcu read lock range for vendor new idle balance
    Sync the range with upstream code.
    
    Bug: 389488203
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    (cherry picked from https://partner-android-review.googlesource.com/q/commit:37cab7ec1863434cf2eb478ad3d51015f7aea9db)
    Merged-In: I3fa93bdc763473ea873126b2d716327207844b9f
    Change-Id: I3fa93bdc763473ea873126b2d716327207844b9f

--- drivers/soc/google/vh/kernel/sched/fair.c
+++ drivers/soc/google/vh/kernel/sched/fair.c
@@ -2643,7 +2643,6 @@ static struct task_struct *detach_important_task(struct rq *src_rq, int dst_cpu)
 
 	lockdep_assert_held(&src_rq->__lock);
 
-	rcu_read_lock();
 
 	list_for_each_entry_reverse(p, &src_rq->cfs_tasks, se.group_node) {
 		bool is_ui = false, is_boost = false;
@@ -2720,7 +2719,6 @@ static struct task_struct *detach_important_task(struct rq *src_rq, int dst_cpu)
 			cpu_rq(dst_cpu)->misfit_task_load = 0;
 	}
 
-	rcu_read_unlock();
 	return p;
 }
 
@@ -2825,6 +2823,8 @@ void sched_newidle_balance_pixel_mod(void *data, struct rq *this_rq, struct rq_f
 			continue;
 		}
 
+		rcu_read_lock();
+
 		p = detach_important_task(src_rq, this_cpu);
 
 		rq_unlock(src_rq, &src_rf);
@@ -2832,10 +2832,12 @@ void sched_newidle_balance_pixel_mod(void *data, struct rq *this_rq, struct rq_f
 		if (p) {
 			attach_one_task(this_rq, p);
 			local_irq_restore(src_rf.flags);
+			rcu_read_unlock();
 			break;
 		}
 
 		local_irq_restore(src_rf.flags);
+		rcu_read_unlock();
 	}
 
 	raw_spin_rq_lock(this_rq);
